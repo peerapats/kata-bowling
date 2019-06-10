2531ec758f71cb23e16e479fc9bec19eadf12865
teeraysi <teeraysi@scg.com>
2019-06-10 18:01:47 +0700

    teamT

--- main.go
+++ main.go
@@ -3,12 +3,30 @@ package main
 import "fmt"
 
 func main() {
+
+	input := [10]string{
+		"x,-",
+		"x,-",
+		"x,-",
+		"x,-",
+		"x,-",
+		"x,-",
+		"x,-",
+		"x,-",
+		"x,-",
+		"xxx",
+	}
+
+	// for index, name := range input {
+
+	// }
+
+	fmt.Println(input)
+
 	scores := [5]string{"9", "0", "9", "1", "10"}
 
 	for index, name := range scores {
-
-
-
 		fmt.Println(index, name)
 	}
+
 }
