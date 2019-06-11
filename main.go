d2d5cbcf204b0edc252708821b1df04d8e4f6b40
Gusjib <gusjib.j@gmail.com>
2019-06-11 17:34:14 +0700

    loop sum

--- main.go
+++ main.go
@@ -2,13 +2,28 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
+// func cal(val string, next string) {
+// 	points := strings.Split(val, "")
+// 	frame := 0
+// 	for key, value := range points {
+// 		if ()
+// 	}
+// }
+
 func main() {
-	var games = []string{"2/", "x", "x", "x", "x", "x", "x", "x", "x", "xxx"}
-	score := 0
-	for index, value := range games {
-		fmt.Println(index, " = ", value)
+	var games = [...]string{"24", "02", "23", "15", "13", "10", "53", "63", "27", "11"}
+	fmt.Println("Input => ", games)
+	totalScore := int64(0)
+	for key, value := range games {
+		i1, _ := strconv.ParseInt(string(value[0]), 10, 64)
+		i2, _ := strconv.ParseInt(string(value[1]), 10, 64)
+		frameScore := int64(0)
+		frameScore = i1 + i2
+		totalScore += frameScore
+		fmt.Println("Frame", key+1, " = ", i1+i2)
 	}
-	fmt.Println(score)
+	fmt.Println(totalScore)
 }
