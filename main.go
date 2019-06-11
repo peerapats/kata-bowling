5aba950807466a08cf24b7b9cdf27d3725cbeec7
teeraysi <teeraysi@scg.com>
2019-06-11 16:57:28 +0700

    commit

--- main.go
+++ main.go
@@ -1,32 +1,92 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
+
+// เก็บคะแนนของแต่ละเกมส์
+var score [10]int
+
+// เก็บตัวทดการสะสมคะแนน
+var carry [10]int
+
+var input = [10]string{
+	"X-",
+	"X-",
+	"X-",
+	"X-",
+	"X-",
+	"X-",
+	"X-",
+	"X-",
+	"X-",
+	"XXX",
+}
 
 func main() {
 
-	input := [10]string{
-		"x,-",
-		"x,-",
-		"x,-",
-		"x,-",
-		"x,-",
-		"x,-",
-		"x,-",
-		"x,-",
-		"x,-",
-		"xxx",
+	fmt.Println("Input: ", input)
+
+	for gameNoIndex, value := range input {
+
+		for _, ch := range value {
+			str := string(ch)
+
+			var scoreValue int
+			if str == "X" || str == "/" {
+				scoreValue = 10
+			} else if str == "-" {
+				scoreValue = 0
+			} else {
+				iValue, _ := strconv.ParseInt(str, 10, 32)
+				scoreValue = int(iValue)
+			}
+
+			// Fullfill score to preivos Strike or Spare game
+			fullfillScore(scoreValue)
+
+			// Keep current score value.
+			score[gameNoIndex] += scoreValue
+
+			// Keep carry fullfill score to current Game
+			if gameNoIndex < len(input)-1 {
+				if str == "X" {
+					carry[gameNoIndex] = 2
+					break
+				} else if str == "/" {
+					carry[gameNoIndex] = 1
+				}
+			}
+		}
+
 	}
 
-	// for index, name := range input {
+	showScore()
+	fmt.Println("Total Score: ", sumScore())
+}
 
-	// }
+func fullfillScore(scoreValue int) {
+	for index, value := range carry {
+		if value > 0 {
+			score[index] += scoreValue
+			carry[index]--
+		}
+	}
+}
 
-	fmt.Println(input)
+func showScore() {
+	for index, value := range score {
+		fmt.Printf("Frame %d: %d\n", index+1, value)
+		// fmt.Println("Frame %d: %d", index+1, value)
+	}
+}
 
-	scores := [5]string{"9", "0", "9", "1", "10"}
-
-	for index, name := range scores {
-		fmt.Println(index, name)
+func sumScore() int {
+	totalScore := 0
+	for _, val := range score {
+		totalScore += val
 	}
 
+	return totalScore
 }
