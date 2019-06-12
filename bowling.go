d1ef746ae872473149e5fe0a89525e9e25c57237
Wanatchapong Bew <bewbangluang@gmail.com>
2019-06-12 16:59:25 +0700

    create bowling.go

---
+++ bowling.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type frame struct {
+	pins_1   string
+	pins_2   string
+	pins_3   string
+	score    int
+	isStrike bool
+	isSpare  bool
+}
+
+func isNumeric(s string) bool {
+	_, err := strconv.ParseFloat(s, 64)
+	return err == nil
+}
+
+func main() {
+	rolls := []string{"24", "x", "3/", "12", "x", "5/", "x", "x", "44", "xxx"}
+	size := len(rolls)
+
+	var frames []frame
+	for i := 0; i < size; i++ {
+		pins := strings.Split(rolls[i], "")
+
+		var f = new(frame)
+		f.pins_1 = pins[0]
+
+		if f.pins_1 == "x" {
+			f.score = 10
+			f.isStrike = true
+		} else if isNumeric(f.pins_1) {
+			p1_score, _ := strconv.Atoi(f.pins_1)
+			f.score = p1_score
+		}
+
+		if len(pins) > 1 {
+			f.pins_2 = pins[1]
+			if f.pins_2 == "/" {
+				f.score = 10
+				f.isSpare = true
+			} else if isNumeric(f.pins_2) {
+				p2_score, _ := strconv.Atoi(f.pins_2)
+				f.score = f.score + p2_score
+			}
+		}
+
+		frames = append(frames, *f)
+
+		if i != 0 && frames[i-1].isStrike {
+			frames[i-1].score = frames[i-1].score + f.score
+		}
+	}
+
+	for j := 0; j < len(frames); j++ {
+		fmt.Println("Frame ", j+1, " = ", frames[j].score)
+	}
+}
