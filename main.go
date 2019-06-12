5fdc05c467b157ef85cf25766907301f0855712d
Tanasit N <tanasit.work@gmail.com>
2019-06-12 17:33:58 +0700

    Calculate except last frame

--- main.go
+++ main.go
@@ -6,26 +6,36 @@ import (
 	"strconv"
 )
 
-var score1 = [10]string{"x", "x", "x", "x", "x", "x", "x", "x", "x", "xxx"}
-var score2 = [10]string{"4/", "5/", "x", "-/", "--", "--", "--", "--", "--", "7/8"}
-var score3 = [10]string{"-/", "6-", "x", "71", "8-", "x", "6/", "7/", "-/", "x4/"}
+var score0 = [10]string{"x-", "x-", "x-", "x-", "x-", "x-", "x-", "x-", "x-", "xxx"}
+var score1 = [10]string{"x-", "x-", "x-", "x-", "x-", "x-", "x-", "x-", "x-", "12"}
+var score2 = [10]string{"4/", "5/", "x-", "-/", "--", "--", "--", "--", "--", "78"}
+var score3 = [10]string{"-/", "6-", "x-", "71", "8-", "x-", "6/", "7/", "-/", "33"}
 
-var score = [10]string{"x", "12", "12", "12", "12", "12", "12", "12", "12", "12"}
+var score = [10]string{"x-", "1/", "12", "12", "12", "12", "12", "12", "12", "12"}
 
 func main() {
-	fmt.Println("Hello Golang!")
-	calc(score)
+	// fmt.Println("Hello Golang!")
+	calc(score0)
 }
 
 func calc(scores [10]string) {
-	// fmt.Println(scores)
+	fmt.Println(scores)
 	// sum := 0
 	for i := range scores {
 		// fmt.Println(scores[i])
 		s := strings.Split(scores[i], "")
 		// fmt.Println(s)
-		throwing(s, scores, i)
+
+		if i == 9 {
+			throwLast(s, scores, i)
+		} else {
+			throwing(s, scores, i)
+		}
 	}
+}
+
+func throwLast(s []string, scores [10]string, index int) {
+	
 }
 
 func throwing(s []string, scores [10]string, index int) {
@@ -36,15 +46,63 @@ func throwing(s []string, scores [10]string, index int) {
 			sum = 0
 		case "x":
 			s2 := strings.Split(scores[index + 1], "")
-			sum = 10 + convertStringToInt(s2[0]) + convertStringToInt(s2[1])
+			sum = calculateStrike(s2, scores, index)
 		case "1", "2", "3", "4", "5", "6", "7", "8", "9" :
 			sum = convertStringToInt(s[0])
 	}
 
+	switch s[1] {
+		case "-":
+			sum = sum + 0
+		case "1", "2", "3", "4", "5", "6", "7", "8", "9" :
+			sum = sum + convertStringToInt(s[1])
+		case "/":
+			s2 := strings.Split(scores[index + 1], "")
+			sum = calSpare(s2)
+	}	
+
 	fmt.Println(sum)
 }
 
+func calSpare(s2 []string) int {
+	if s2[0] == "x" {
+		return 20
+	} else {
+		return 10 + convertStringToInt(s2[0])
+	}
+}
+
+func calculateStrike(s2 []string, scores [10]string, index int) int {
+	if s2[0] == "x" {
+		if index != 8 {
+			s3 := strings.Split(scores[index + 2], "")
+			if s3[0] == "x" {
+				return 30
+			} else {
+				return 20 + convertStringToInt(s3[0])
+			}
+		} else {
+			return 20 + convertStringToInt(s2[1])
+		}
+		
+	} else {
+		if s2[1] == "/" {
+			return 20
+		} else {
+			return 10 + convertStringToInt(s2[0]) + convertStringToInt(s2[1])
+		}
+	}
+}
+
 func convertStringToInt(str string) int {
+	if str == "-" {
+		return 0
+	}
+
+	if str == "x" {
+		return 10
+	}
+
 	i1, err := strconv.Atoi(str)
 	if err == nil {
 		return i1
@@ -52,5 +110,3 @@ func convertStringToInt(str string) int {
 		return 0
 	}
 }
-
-// func summary()
