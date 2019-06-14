caf1dfd65f18727c0ce7b82022cfc16ac7f01853
ratchanon <fomersss@hotmail.com>
2019-06-14 17:17:24 +0700

    asdaaa

--- main.go
+++ main.go
@@ -1,18 +1,166 @@
-package main
 
-import "fmt"
+package main 
 
-func main() {
-	scores := [5]string{"9", "0", "9", "1", "10"}
+import ( 
+"fmt" 
+"strconv" 
+"unicode/utf8" 
+"strings" 
+) 
 
-	for index, name := range scores {
+func getScore(input []string) (int, []int) { 
+var score []int 
+var total int 
+for i, element := range input { 
+var sum int 
 
+if(i == 9 ) { 
+var a int; 
+var b int; 
+var c int; 
+a=0
+b=0
+c=0
+res := strings.Split(element, "") 
+if(res[0] == "x"){
+	a= 10
+}else{
+	a,_ = strconv.Atoi(res[0]); 
+}
 
+if(res[1] == "x"){
+	b= 10
+}else if(res[1] == "/"){
+	b = 10 - a
+}else{
+	b,_ = strconv.Atoi(res[1]); 
+}
+if(len(element) == 3){
+	if(res[2] == "x"){
+	  c = 10
+	}else{
+	  c,_ = strconv.Atoi(res[2]); 
+	}
+}
+var sum = a+b+c; 
+score = append(score, sum); 
+} else { 
 
-		fmt.Println(index, name)
+if(element == "x") { 
+
+sum = 10; 
+score = append(score, 10); 
+// fmt.Println("input",input[i+1]) 
+// ให้เอา 2 ตาต่อไปมาบวก 
+//บวกรวบแรก 
+if(input[i+1] == "x") {
+score[i] += 10; 
+var isSet = false; 
+//เหลืออีก 2 รอบ 
+if(i < len(input) - 2) && (input[i+2][0] == 'x'){ 
+score[i] += 10; 
+} else { 
+for _, c := range input[i+2] { 
+buf := make([]byte, 1) 
+_ = utf8.EncodeRune(buf, c) 
+value, _ := strconv.Atoi(string(buf)) 
+if(!isSet) { 
+score[i] += value; 
+isSet = true; 
+} 
+} 
+} 
+} else { 
+for _, c := range input[i+1] { 
+buf := make([]byte, 1) 
+_ = utf8.EncodeRune(buf, c) 
+value, _ := strconv.Atoi(string(buf)) 
+score[i] += value; 
+} 
+} 
+
+if(i == 8){
+	res := strings.Split(input[i+1], "") 
+	var a int; 
+    a=0
+	if(input[i] == "x") {
+		score[i] = 10;
+		if(res[0] == "x"){
+			score[i] += 10;	
+		} else {
+			a,_ = strconv.Atoi(res[0]); 
+			score[i] += a
+		}
+		if(res[1] == "x"){
+			score[i] += 10;	
+		}else if(res[1] == "/"){
+			a,_ = strconv.Atoi(res[0]); 
+			score[i] += 10 - a;	
+		}else{
+			a,_ = strconv.Atoi(res[1]); 
+			score[i] += a;	
+		}
 	}
 }
 
-func AlwaysReturn1() int {
-	return 1
+
+} else { 
+var isSp = false; 
+for _, c := range element { 
+buf := make([]byte, 1) 
+_ = utf8.EncodeRune(buf, c) 
+value, _ := strconv.Atoi(string(buf)) 
+
+if(c == '/') { 
+isSp = true; 
+sum = 10; 
+} else { 
+sum += value; 
+} 
+
+} 
+
+score = append(score, sum); 
+
+if(isSp && i <= 9) { 
+if(i < len(input) - 1) && (input[i+1] == "X"){ 
+score[i] += 10; 
+} else { 
+var isSet = false; 
+for _, c := range input[i+1] { 
+buf := make([]byte, 1) 
+_ = utf8.EncodeRune(buf, c) 
+value, _ := strconv.Atoi(string(buf)) 
+if(!isSet) { 
+score[i] += value; 
+isSet = true; 
+} 
+} 
+} 
+} 
+} 
+
+} 
+
+} 
+
+// fmt.Println("Input",input); 
+for _, v := range score { 
+total += v 
+//fmt.Println("Frame ", i + 1 ,"[" , input[i], "] : ", v); 
+} 
+//fmt.Println("total ", total); 
+return total, score; 
+} 
+
+func main () { 
+	// var input = []string{"--", "--", "1/", "--", "--", "--", "--", "--", "--", "--"} 
+	var input = []string{"--", "--", "--", "--", "--", "--", "--", "--", "x", "x11"}
+
+	 total,score :=getScore(input)
+	 fmt.Println(input)
+	 for i, v := range score {  
+		fmt.Println("Frame ", i + 1 ,"[" , input[i], "] : ", v); 
+		} 
+		fmt.Println("Total is ",total)
 }
--- main_test.go
+++ main_test.go
@@ -1,20 +1,111 @@
-package main
+package main 
 
-import "testing"
-import "github.com/stretchr/testify/assert"
+import "testing" 
+// import "github.com/stretchr/testify/assert" 
 
-type Person struct {
-	name string
-}
+type Person struct { 
+	name string 
+	} 
+			func TestAlwaysReturn0(t *testing.T) { 
+				var input = []string{"--", "--", "--", "--", "--", "--", "--" , "--" , "--" , "--"} 
+				result,_ := getScore(input) 
+				if result != 0 { 
+				t.Error("Result should be 0") 
+				} 
+				} 
 
-func TestAlwaysReturn1(t *testing.T) {
-	result := AlwaysReturn1()
-	if result != 1 {
-		t.Error("Result should be 1")
-	}
-}
+func TestAlwaysReturnOpen(t *testing.T) { 
+var input = []string{"11", "11", "11", "11", "11", "11", "11" , "11" , "11" , "11"} 
+result,_ := getScore(input) 
+if result != 20 { 
+t.Error("Result should be 20") 
+} 
+} 
 
-func TestAlwaysReturn1WithAssert(t *testing.T) {
-	result := AlwaysReturn1()
-	assert.Equal(t, 1, result, "Result should be 1")
-}
+func TestAlwaysReturnSpare(t *testing.T) { 
+var input = []string{"--", "--", "1/", "--", "--", "--", "--", "--", "--", "--"} 
+result,_ := getScore(input) 
+if result !=10 { 
+t.Error("Result should be 10") 
+} 
+} 
+
+func TestAlwaysReturnDoubleSpare(t *testing.T) { 
+	var input = []string{"--", "--", "1/", "1/", "--", "--", "--", "--", "--", "--"} 
+	result,_ := getScore(input) 
+	if result !=21 { 
+	t.Error("Result should be 21") 
+	} 
+	} 
+
+func TestAlwaysReturnStrike(t *testing.T) { 
+	var input = []string{"--", "--", "--", "--", "x", "--","--","--","--","--"} 
+	result,_ := getScore(input) 
+	if result != 10 { 
+	t.Error("Result should be 10") 
+	} 
+	} 
+
+func TestAlwaysReturnDoubleStrike(t *testing.T) { 
+var input = []string{"--", "--", "--", "--", "x", "x","--","--","--","--"} 
+result,_ := getScore(input) 
+if result != 30 { 
+t.Error("Result should be 30") 
+} 
+} 
+
+
+	
+
+func TestAlwaysReturnAllStrike(t *testing.T) { 
+var input = []string{"x", "x", "x", "x", "x", "x", "x", "x", "x", "xxx"}
+result,_ := getScore(input) 
+if result != 300 { 
+t.Error("Result should be 300") 
+} 
+} 
+
+func TestAlwaysReturnLastFrameStrike(t *testing.T) { 
+	var input = []string{"--", "--", "--", "--", "--", "--", "--", "--", "--", "x12"}
+	result,_ := getScore(input) 
+	if result != 13 { 
+	t.Error("Result should be 13") 
+	} 
+	} 
+
+	func TestAlwaysReturnSpareLastFrameSpare(t *testing.T) { 
+		var input = []string{"--", "--", "--", "--", "--", "--", "--", "--", "--", "1/2"}
+		result,_ := getScore(input) 
+		if result != 12 { 
+		t.Error("Result should be 12") 
+		} 
+	} 
+
+		func TestAlwaysReturnFrame9Strike(t *testing.T) { 
+			var input = []string{"--", "--", "--", "--", "--", "--", "--", "--", "x", "x11"}
+			result,_ := getScore(input) 
+			if result != 33 { 
+			t.Error("Result should be 33") 
+			} 
+			} 
+
+			func TestAlwaysReturnFrame9Spare(t *testing.T) { 
+				var input = []string{"--", "--", "--", "--", "--", "--", "--", "--", "1/", "22"}
+				result,_ := getScore(input) 
+				if result != 16  { 
+				t.Error("Result should be 16") 
+				} 
+				} 
+			func TestAlwaysReturnFrame9Open(t *testing.T) { 
+				var input = []string{"--", "--", "--", "--", "--", "--", "--", "--", "11", "--"}
+				result,_ := getScore(input) 
+				if result != 2  { 
+				t.Error("Result should be 2") 
+				} 
+				} 
+
+// func TestAlwaysReturn43WithAssert(t *testing.T) { 
+// var input = []string{"22", "44", "1/", "12", "x", "52"} 
+// result,_ := getScore(input) 
+// assert.Equal(t, 43, result, "Result should be 43") 
+// }
